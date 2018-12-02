bcc3bf8501a0ff022174ac1f365918fcb01cd738
lichaozhy <lichao@or-change.cn>
2018-12-03 00:34:07 +0800

    [FIXED] top10

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -43,7 +43,7 @@ module.exports = {
 				loader: 'url-loader',
 				options: {
 					limit: 10000,
-					name: path.join(assetsPath, 'fonts/[name].[hash:7].[ext]')
+					name: path.join('[name].[hash:7].[ext]')
 				}
 			},
 			{
